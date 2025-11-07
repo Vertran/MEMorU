70c591911842c25871eaab322fe00807b02e4a02
Chloroformik <121249266+Vertran@users.noreply.github.com>
2025-11-07 22:33:01 +0100

    save

--- game/game.cpp
+++ game/game.cpp
@@ -18,4 +18,25 @@ int main() {
     Logger logger("C:/Games/MEMorU/game/data/game.log");
 
     logger.info("Startup!");
+
+
+    logger.info("So... You've launched this game...");
+    logger.info("Do you know what exactly you have launched? (y/n)");
+
+    string userAnswer;
+    cin >> userAnswer;
+    logger.info("Your answer is..." + userAnswer);
+
+    if (userAnswer == "y") {
+		logger.info("No, you don't know.");
+    }
+    else {
+        logger.info("So... Now I'll tell you...");
+    }
+
+    logger.warning("Launching supercollider.");
+    logger.warning("Launching reactor.");
+    logger.error("reactor may not function properly. DEACTIVATING IT.");
 }
+
+main();
--- game/modules/entity.cpp
+++
@@ -1,5 +0,0 @@
-#include 
-
-class Entity {
-
-};
